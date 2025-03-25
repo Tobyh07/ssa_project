71ade4d0c41d113c175c9eb76079f661922d7353
toby.haasnoot <toby.haasnoot2004@gmail.com>
2025-03-25 14:35:33 +1100

    Group Join Requests

--- ssa_project/users/views.py
+++ ssa_project/users/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegistrationForm
 
+
 def register(request):
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
